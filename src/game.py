5e8c87a7d690d1969fdb00fc1ff7245fabf292eb
dphennessy <dphennessy2@gmail.com>
2019-03-27 16:21:10 +0000

    Changed paths to use Config and added comments

--- src/game.py
+++ src/game.py
@@ -5,36 +5,46 @@ import json
 from config import Config
 
 
+# Class meant to be inherited by anything that should display a tooltip when hovered over
 class Hover:
     def __init__(self, name, description, icon=Config['resources']['ui']['icons']['default']):
-        font = pg.font.Font("/home/david/Documents/BrassLands/src/res/font.ttf", 30)
+        # Fetches resources needed
+        font = pg.font.Font(Config['resources']['ui']['fonts']['tooltip'], 30)
         self.icon = pg.image.load(icon)
-        self.surf = pg.image.load("/home/david/Documents/BrassLands/src/res/hover.png")
+        self.surf = pg.image.load(Config['resources']['ui']['hover'])  # main surface
         self.rect = self.surf.get_rect()
 
+        # Sets the name and description of tooltip
         name = "<Item Name>" if name is None else name
         description = "<Item Description>" if description is None else description
 
+        # Generates surfaces for the name and description
         self.name_surf = font.render(name, True, (235, 235, 235))
         self.desc_surf = font.render(description, True, (235, 235, 235))
 
+        # Blits the icon, name, and description onto the main surface
         self.surf.blit(self.icon, (13, 13))
         self.surf.blit(self.name_surf, (65, 15))
         self.surf.blit(self.desc_surf, (13, 55))
 
+    # Blits the tooltip onto a surface at the desired position
     def show_tooltip(self, surface, pos):
         self.rect.bottomright = pos
         surface.blit(self.surf, self.rect)
 
 
+# Class meant to be inherited by items
 class Item(Hover):
     def __init__(self, j_string):
         self.name, self.description = None, None
-        obj = json.loads(j_string)
-        self.parse_json(obj)
-        super().__init__(self.name, self.description)
+        self.parse_json(j_string)
+        super().__init__(self.name, self.description)  # Initializes the tooltip for this item
 
-    def parse_json(self, obj):
+    # Takes in a json string and attempts to get the name and description of the item
+    # Classes inheriting Item can call this to set the name and description
+    # and then should implement their own parse_json method
+    def parse_json(self, j_string):
+        obj = json.loads(j_string)
         try:
             self.name = obj['name']
             self.description = obj['description']
@@ -44,55 +54,55 @@ class Item(Hover):
 
 class Inventory:
     def __init__(self):
-        w, h = 7, 3
+        w, h = 7, 3  # Number of rows and columns in the visible inventory
+        self.capacity = w * h  # Max capacity of inventory
         self.visible = False
-        self.surface = pg.image.load(Config['resources']['ui']['inventory'])
+        self.surface = pg.image.load(Config['resources']['ui']['inventory'])  # main surface
         self.rect = self.surface.get_rect()
-        self.rect.bottomright = (Config['game']['width'], Config['game']['height'])
+        self.rect.bottomright = (Config['game']['width'], Config['game']['height'])  # Positions the inventory
         x, y = 20, 0
+        # Creates a Rect for each slot in the visible inventory
         self.inv_rects = [pg.Rect((x + i * 42, y + 290 + j * 42), (40, 40)) for j in range(h) for i in range(w)]
+        self.items = []  # List to hold the item objects currently in inventory
 
-        self.items = [None for _ in range(21)]
-        
-    # noinspection PyTypeChecker
+    def get_items(self):
+        return self.items
+
+    # Adds an item to the inventory as long as it doesn't exceed the maximum capacity of the inventory
     def add(self, item):
-        if isinstance(item, Item):
-            for i, e in enumerate(self.items):
-                if e is None:
-                    self.items[i] = item
-                    self.update()
-                    return True
+        if isinstance(item, Item) and len(self.items) < self.capacity:
+            self.items.append(item)
+            self.update()
+            return True
         return False
 
+    # Removes the specified item from the inventory
     def remove(self, item):
+        self.surface = pg.image.load(
+            Config['resources']['ui']['inventory'])  # reloads the image to remove previously draw icons
         self.items.remove(item)
         self.update()
+
+    # Method called to redraw the inventory onto the main surface
+    def update(self):
+        for i, item in enumerate(self.items):
+            self.surface.blit(item.icon, self.inv_rects[i])
 
     def event_handler(self, event):
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_i:
                 self.visible = not self.visible
 
-    def update(self):
-        self.surface = pg.image.load(Config['resources']['ui']['inventory'])
-        for i, item in enumerate(self.items):
-            if item is not None:
-                self.surface.blit(item.icon, self.inv_rects[i])
-
     def draw(self, surface):
         if self.visible:
             surface.blit(self.surface, self.rect)
             mouse_pos = pg.mouse.get_pos()
-            rel_pos = (mouse_pos[0] - self.rect.topleft[0], mouse_pos[1] - self.rect.topleft[1])
-            for i, rect in enumerate(self.inv_rects):
-                if rect.collidepoint(rel_pos):
-                    if self.items[i] is not None:
-                        self.items[i].show_tooltip(surface, mouse_pos)
-
-        # x = 0
-        # for rect in self.inv_rects:
-        #     pg.draw.rect(self.surface, (255 - x, 255 - x / 2, 255 - x - 5), rect)
-        #     x += 10
+            rel_pos = tuple(  # relative position to 0,0 coordinate of main surface
+                map(lambda m, r: m - r, mouse_pos, self.rect.topleft))  # Subtracts mouse_pos from  rect.topleft
+            # Draws tooltip of item on mouseover
+            for i, item in enumerate(self.items):
+                if self.inv_rects[i].collidepoint(rel_pos):
+                    item.show_tooltip(surface, mouse_pos)
 
 
 class Ground(pg.sprite.Sprite):
@@ -215,7 +225,6 @@ class Game:
 
         self.coord_grid = make_grid()
         self.scenery_grid = make_level(self.scenery_grp, self.coord_grid, Config['resources']['levels']['level1'])
-        print(Config['resources']['sprites']['player'])
         self.char = Player(Config['resources']['sprites']['player'], self.coord_grid)  # Initializes player at (0, 0)
         self.player_grp.add(self.char)
 
